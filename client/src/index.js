238435b7dfe28c336e5eb7cca5329cf384415fce
ShiwaniTukaramDaspute <83324114+ShiwaniTukaramDaspute@users.noreply.github.com>
2021-10-19 14:38:17 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
+//Import css file
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import {Provider} from "react-redux";
 import Store from "./Redux/store";
 
@@ -13,7 +17,7 @@ ReactDOM.render(
   <BrowserRouter>
     <App />
     </BrowserRouter>
-  <Provider />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
