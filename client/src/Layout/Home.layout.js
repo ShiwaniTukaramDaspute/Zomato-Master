40be0d2e9e970b678539f1dd727d9a6e72897941
ShiwaniTukaramDaspute <83324114+ShiwaniTukaramDaspute@users.noreply.github.com>
2021-10-19 14:40:06 +0530

    Update Home.layout.js

--- client/src/Layout/Home.layout.js
+++ client/src/Layout/Home.layout.js
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
 //Components
 import Navbar from "../Components/Navbar";
 import FoodTab from "../Components/FoodTab";
 
+//Redux Actions
+import {getRestaurant} from "../Redux/Reducer/restaurant/restaurant.action";
+
 const HomeLayout = (props) => {
+const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getRestaurant());
+  },[]);
+
   return <>
    <div className="container mx-auto lg:px-20">
    <Navbar />
+   <FoodTab />
     {props.children}
    </div>
-   <FoodTab />
   </>
 };
 
